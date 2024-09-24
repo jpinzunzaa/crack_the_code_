5237d460426c19dd939f4e7eb3f2bd68550c993e
Juan Inzunza <johnny.phocker@gmail.com>
2024-09-24 09:13:38 -0700

    Fixed code

--- gauss-sum/index.test.js
+++ gauss-sum/index.test.js
@@ -2,13 +2,13 @@ const { gauss_sum } = require('./index');
 
 describe('Cálculo de la suma de Gauss', () => {
   test('Debe calcular correctamente la suma de Gauss para un número positivo', () => {
-    expect(gaussSum(5)).toBe(15);
-    expect(gaussSum(10)).toBe(55);
+    expect(gauss_sum(5)).toBe(15);
+    expect(gauss_sum(10)).toBe(55);
   });
 
   test('Debe lanzar un error para números no válidos', () => {
-    expect(() => gaussSum(-5)).toThrow('Ingrese un número entero positivo.');
-    expect(() => gaussSum(0)).toThrow('Ingrese un número entero positivo.');
-    expect(() => gaussSum(3.5)).toThrow('Ingrese un número entero positivo.');
+    expect(() => gauss_sum(-5)).toThrow('Ingrese un número entero positivo.');
+    expect(() => gauss_sum(0)).toThrow('Ingrese un número entero positivo.');
+    expect(() => gauss_sum(3.5)).toThrow('Ingrese un número entero positivo.');
   });
 });
