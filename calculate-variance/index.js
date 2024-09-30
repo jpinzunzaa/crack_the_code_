4f34ae91a4a191b33b9ad0f417f63dc40afea0c1
Juan Inzunza <jp@catapulta.digital>
2024-09-29 18:25:23 -0700

    Cleaned code

--- calculate-variance/index.js
+++ calculate-variance/index.js
@@ -3,11 +3,10 @@ exports.calculate_variance= (numbers) => {
     throw new Error('Debe proporcionar un conjunto de números');
   }
 
-
   const mean = numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
 
   const squared_diffs = numbers.map(num => Math.pow(num - mean, 2));
   const variance = squared_diffs.reduce((sum, diff) => sum + diff, 0) / numbers.length;
-
+ 
   return variance;
 }
